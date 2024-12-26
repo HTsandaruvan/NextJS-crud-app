9baf196a6aa6d2c7085d512fdffff66cc773ba84
Harsha Tharuka <108293334+HTsandaruvan@users.noreply.github.com>
2024-12-27 01:11:38 +0530

    Loginrequest

--- app/login/login-form.jsx
+++ app/login/login-form.jsx
@@ -1,5 +1,6 @@
-'use client'
-import React, { useState } from 'react'
+"use client";
+import React, { useState } from "react";
+import { loginUser } from "@/app/libs/apis/server";
 
 const LoginForm = () => {
     const [email, setEmail] = useState("");
@@ -11,71 +12,125 @@ const LoginForm = () => {
             setemailError("Email is required");
             return false;
         } else {
-            setemailError("")
+            setemailError("");
         }
         if (!password) {
             setPasswordError("Password is required");
             return false;
         } else {
-            setPasswordError("")
+            setPasswordError("");
         }
 
         return true;
-    }
-    const handleSubmit = (e) => {
-
+    };
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const isValid = validateForm()
-        if (!isValid) {
-            console.log(email)
-            console.log(password)
+
+        const isValid = validateForm();
+
+        if (isValid) {
+            // Login Form Data Submission
+            const login = await loginUser({ email: email, password: password });
+
+            console.log("LOGIN RESPONSE", login);
         }
-    }
+    };
     return (
-        <div className='min-h-screen flex items-center justify-center'>
-
-            <div className='w-[380px] mx-auto '>
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="w-[380px] mx-auto ">
                 <div className="flex items-center justify-center my-3">
-                    <h1 className='font-semibold text-2xl'>
-                        Evotech
-                    </h1>
+                    <h1 className="font-semibold text-2xl">Evotech</h1>
                 </div>
-                <div className='bg-white border border-gray-200 shadow-md p-4 rounded-lg'>
-                    <form onSubmit={handleSubmit} action={""} className='space-y-6'>
-                        <h1 className='text-start text-xl font-bold text-gray-900'>Sign in to your account</h1>
+                <div className="bg-white border border-gray-200 shadow-md p-4 rounded-lg">
+                    <form onSubmit={handleSubmit} action={""} className="space-y-6">
+                        <h1 className="text-start text-xl font-bold text-gray-900">
+                            Sign in to your account
+                        </h1>
                         <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                            <input onChange={(e) => setEmail(e.target.value)} value={email} className='bg-gray-50 border border-gray-300 text-gray-900   block w-full p-1.5 rounded-md' type="text" name='email' id='email' placeholder="name@example.com" required="" />
-                            {emailError && <div className='text-red-500 text-sm p-1.5'>{emailError}</div>}
+                            <label
+                                htmlFor="email"
+                                className="block mb-2 text-sm font-medium text-gray-900"
+                            >
+                                Your email
+                            </label>
+                            <input
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                                className="bg-gray-50 border border-gray-300 text-gray-900   block w-full p-1.5 rounded-md"
+                                type="text"
+                                name="email"
+                                id="email"
+                                placeholder="name@example.com"
+                                required=""
+                            />
+                            {emailError && (
+                                <div className="text-red-500 text-sm p-1.5">{emailError}</div>
+                            )}
                         </div>
                         <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                            <input onChange={(e) => setPassword(e.target.value)} value={password} className='bg-gray-50 border border-gray-300 text-gray-900   focus:ring-blue-600 focus:border-blue-600 block w-full p-1.5 rounded-md' type="password" name='password' id='password' placeholder="xxxxxxx" required="" />
-                            {passwordError && <div className='text-red-500 text-sm p-1.5'>{passwordError}</div>}
-
-                        </div>
-                        <div className='flex justify-between'>
-                            <div className='flex'>
-                                <div className='flex items-center h-5'>
-                                    <input id='remember' type="checkbox" />
+                            <label
+                                htmlFor="password"
+                                className="block mb-2 text-sm font-medium text-gray-900"
+                            >
+                                Password
+                            </label>
+                            <input
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                className="bg-gray-50 border border-gray-300 text-gray-900   focus:ring-blue-600 focus:border-blue-600 block w-full p-1.5 rounded-md"
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="xxxxxxx"
+                                required=""
+                            />
+                            {passwordError && (
+                                <div className="text-red-500 text-sm p-1.5">
+                                    {passwordError}
                                 </div>
-                                <div className='text-sm ml-2'>
-                                    <label htmlFor="remember" className='font-medium text-gray-900'>Remember Me</label>
+                            )}
+                        </div>
+                        <div className="flex justify-between">
+                            <div className="flex">
+                                <div className="flex items-center h-5">
+                                    <input id="remember" type="checkbox" />
+                                </div>
+                                <div className="text-sm ml-2">
+                                    <label
+                                        htmlFor="remember"
+                                        className="font-medium text-gray-900"
+                                    >
+                                        Remember Me
+                                    </label>
                                 </div>
                             </div>
-                            <a href='/forget-password' className='text-sm text-blue-700 hover:underline '>Forget password</a>
+                            <a
+                                href="/forget-password"
+                                className="text-sm text-blue-700 hover:underline "
+                            >
+                                Forget password
+                            </a>
                         </div>
-                        <button type='submit' className='w-full text-white bg-blue-600 hover:bg-blue-700 transition-all focus-visible:ring-4 ring-2 focus:ring-blue-300 px-5 py-1.5'>Sign in</button>
-                        <div className='flex text-sm font-medium text-gray-500 space-x-1 justify-center'>
+                        <button
+                            type="submit"
+                            className="w-full text-white bg-blue-600 hover:bg-blue-700 transition-all focus-visible:ring-4 ring-2 focus:ring-blue-300 px-5 py-1.5"
+                        >
+                            Sign in
+                        </button>
+                        <div className="flex text-sm font-medium text-gray-500 space-x-1 justify-center">
                             <span>Don’t have an account yet? </span>
-                            <a className='text-sm text-blue-700 hover:underline' href="/register">Sign up</a>
-
+                            <a
+                                className="text-sm text-blue-700 hover:underline"
+                                href="/register"
+                            >
+                                Sign up
+                            </a>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default LoginForm
+export default LoginForm;
